490334fd0d95e94f78b57b5fb4b886798225b8ea
“aru949” <aryangupta000@yahoo.in>
2021-03-03 18:56:17 +0000

    adding testing script and changing yaml filename

--- test.py
+++ test.py
@@ -3,10 +3,13 @@ import argparse
 import numpy as np
 
 os.system('rm -r K-test')
+os.system('rm -r test_output')
+
 parser = argparse.ArgumentParser()
 parser.add_argument('--img_dir', default='data', help='path to parent folder')
 parser.add_argument('--test_dir', default='test', help='path to test folder')
 parser.add_argument('--k', type = int, default = 1, help = 'number of images')
+parser.add_argument('--g_ckpt', default = '1.pt', help = 'path to generator checkpoint')
 
 config = parser.parse_args()
 os.mkdir('K-test')
@@ -21,4 +24,32 @@ for clas in os.listdir(config.img_dir):
     src = '{}/{}/{}'.format(config.img_dir,clas,img)
     dest = 'K-test/' + clas
     os.system('cp {} {}'.format(src,dest))
+
+#creating output folders
+os.mkdir('test_output')
+for clas in os.listdir(config.img_dir):
+  os.mkdir('test_output/' + clas)
+
+#testing each image
+
+for clas in os.listdir('K-test'):
+  for img in os.listdir(config.test_dir):
+    os.system('python test_k_shot.py --config configs/train.yaml\
+    --ckpt {} --input {}\
+    --class_image_folder {} --output {}'.format(
+      config.g_ckpt,
+      config.test_dir+'/'+img,
+      'K-test' + '/' + clas,
+      'test_output/' +  clas+ '/'  + img
+    ))
+
+
+
+
+
+
+
+
+
+
 
